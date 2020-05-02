ef3675fdb57de6d1b9221b90a14f2a8b9029e6a1
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-02 20:06:14 +0300

    drcc4d1

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5lUElQ0znf1":
+        Script1();
+        break;
+      case "6SnYFDu4KNT":
+        Script2();
+        break;
+      case "6rT1LORtQOT":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
